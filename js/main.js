60a4ea939d034abaa234abdaec2cf90adb697b87
Derek Van Cuyk <dvan_cuyk@hotmail.com>
2016-12-04 15:00:33 -0500

    Added ice and basic collision detection

--- js/main.js
+++ js/main.js
@@ -46,16 +46,34 @@ var SceneGame = Class.create(Scene, {
     penguin.x = game.width/2 - penguin.width/2;
     penguin.y = 280;
     this.penguin = penguin;
+    this.iceTray = new Group; // Coordinates all of the ice boulders
 
     this.addChild(bg);
+    this.addChild(this.iceTray);
     this.addChild(label);
     this.addChild(penguin);
+    this.generateIceTimer = 0;
   },
   handleTouchControl: function(e){
     var laneWidth = 320/3;
     var lane = Math.floor(e.x/laneWidth);
     lane = Math.max(Math.min(2, lane), 0);
     this.penguin.switchToLaneNumber(lane);
+  },
+  update: function(e){
+    this.generateIceTimer += penguindive.convertToSeconds(e.elapsed);
+    if(this.generateIceTimer >= 0.5){
+      this.generateIceTimer -= 0.5;
+      var ice = new Ice(Math.floor(Math.random()*3));
+      this.iceTray.addChild(ice);
+    }
+    for (var i = 0; i < this.iceTray.childNodes.length; i++) {
+      var ice = this.iceTray.childNodes[i];
+      if(ice.intersect(this.penguin)){
+        this.iceTray.removeChild(ice);
+        break;
+      }
+    }
   }
 });
 
@@ -66,6 +84,10 @@ var Penguin = Class.create(Sprite, {
     this.animationDuration = 0;
     this.addEventListener(Event.ENTER_FRAME, this.updateAnimation);
   },
+  switchToLaneNumber: function(lane){
+    var targetX = 160 - this.width / 2 + (lane - 1) * 90;
+    this.x = targetX;
+  },
   updateAnimation: function(e){
     this.animationDuration += penguindive.convertToSeconds(e.elapsed);
     const threshold = .25;
@@ -73,19 +95,17 @@ var Penguin = Class.create(Sprite, {
       this.frame = (this.frame + 1) % 2;
       this.animationDuration -= threshold;
     }
-  },
-  switchToLaneNumber: function(lane){
-    var targetX = 160 - this.width / 2 + (lane - 1) * 90;
-    this.x = targetX;
   }
 });
 
 var Ice = Class.create(Sprite, {
   initialize: function(lane){
     Sprite.apply(this, [48, 49]);
+    this.image = Game.instance.assets[penguindive.resources.ice];
     this.rotationSpeed = 0;
     this.setLane(lane);
     this.addEventListener(Event.ENTER_FRAME, this.update);
+    console.log('created ice');
   },
   setLane: function(lane) {
 	  var game = Game.instance;
@@ -98,10 +118,10 @@ var Ice = Class.create(Sprite, {
   },
   update: function(e){
     var speed = 300;
-
+    console.log('throwing ice');
     this.y += speed * penguindive.convertToSeconds(e.elapsed);
 	  this.rotation += this.rotationSpeed * penguindive.convertToSeconds(e.elapsed);
-    if (this.y > game.height) {
+    if (this.y > Game.instance.height) {
         this.parentNode.removeChild(this);
     }
   }
